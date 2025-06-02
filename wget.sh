a7ccece2dcaecc1791d645159430e444b9196813
Mike Seddon <seddonm1@gmail.com>
2025-06-02 18:30:33 +1000

    rename public to docs

--- wget.sh
+++ wget.sh
@@ -12,17 +12,17 @@ wget \
 --no-parent \
 http://localhost:2368
 
-mv "localhost:2368" public
+mv "localhost:2368" docs
 
-find ./public/ -type f -name '*.html' | xargs sed -i '' -e 's#\.jpgg#.jpg#g'
-find ./public/ -type f -name '*.html' | xargs sed -i '' -e 's#\.jpgpg#.jpg#g'
-find ./public/ -type f -name '*.html' | xargs sed -i '' -e 's#\.jpgjpg#.jpg#g'
-find ./public/ -type f -name '*.html' | xargs sed -i '' -e 's#\.jpg\.jpg#.jpg#g'
-find ./public/ -type f -name '*.html' | xargs sed -i '' -e 's#\.jpegg#.jpeg#g'
-find ./public/ -type f -name '*.html' | xargs sed -i '' -e 's#\.jpegpg#.jpeg#g'
-find ./public/ -type f -name '*.html' | xargs sed -i '' -e 's#\.jpegpeg#.jpeg#g'
-find ./public/ -type f -name '*.html' | xargs sed -i '' -e 's#\.jpegjpeg#.jpeg#g'
-find ./public/ -type f -name '*.html' | xargs sed -i '' -e 's#\.jpeg\.jpeg#.jpeg#g'
-find ./public/ -type f -name '*.html' | xargs sed -i '' -e 's#.pngg#.png#g'
-find ./public/ -type f -name '*.html' | xargs sed -i '' -e 's#.pngng#.png#g'
-find ./public/ -type f -name '*.html' | xargs sed -i '' -e 's#.pngpng#.png#g'
+find ./docs/ -type f -name '*.html' | xargs sed -i '' -e 's#\.jpgg#.jpg#g'
+find ./docs/ -type f -name '*.html' | xargs sed -i '' -e 's#\.jpgpg#.jpg#g'
+find ./docs/ -type f -name '*.html' | xargs sed -i '' -e 's#\.jpgjpg#.jpg#g'
+find ./docs/ -type f -name '*.html' | xargs sed -i '' -e 's#\.jpg\.jpg#.jpg#g'
+find ./docs/ -type f -name '*.html' | xargs sed -i '' -e 's#\.jpegg#.jpeg#g'
+find ./docs/ -type f -name '*.html' | xargs sed -i '' -e 's#\.jpegpg#.jpeg#g'
+find ./docs/ -type f -name '*.html' | xargs sed -i '' -e 's#\.jpegpeg#.jpeg#g'
+find ./docs/ -type f -name '*.html' | xargs sed -i '' -e 's#\.jpegjpeg#.jpeg#g'
+find ./docs/ -type f -name '*.html' | xargs sed -i '' -e 's#\.jpeg\.jpeg#.jpeg#g'
+find ./docs/ -type f -name '*.html' | xargs sed -i '' -e 's#.pngg#.png#g'
+find ./docs/ -type f -name '*.html' | xargs sed -i '' -e 's#.pngng#.png#g'
+find ./docs/ -type f -name '*.html' | xargs sed -i '' -e 's#.pngpng#.png#g'
